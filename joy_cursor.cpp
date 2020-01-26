2ad5f0e8eb269a944be699efdd7926e1ee790381
charlesancheta <cancheta@ualberta.ca>
2020-01-25 17:54:52 -0700

    hello

---
+++ joy_cursor.cpp
@@ -0,0 +1,110 @@
+// ---------------------------------------------------
+// Name : Charles Ancheta
+// ID: 1581672
+// CMPUT 275 , Winter 2020
+//
+// Weekly Assignment 1: Display and Joystick
+// ---------------------------------------------------
+
+#define SD_CS     10
+#define JOY_VERT  A9 // should connect A9 to pin VRx
+#define JOY_HORIZ A8 // should connect A8 to pin VRy
+
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <MCUFRIEND_kbv.h>
+#include <SPI.h>
+#include <SD.h>
+#include "lcd_image.h"
+
+MCUFRIEND_kbv tft;
+
+#define DISPLAY_WIDTH  480
+#define DISPLAY_HEIGHT 320
+#define YEG_SIZE 2048
+
+lcd_image_t yegImage = { "yeg-big.lcd", YEG_SIZE, YEG_SIZE };
+
+#define JOY_CENTER   512
+#define JOY_DEADZONE 64
+#define CURSOR_SIZE  9
+// global variables for cursor position
+int cursorX, cursorY;
+
+void redrawCursor() {
+  tft.fillRect(cursorX - CURSOR_SIZE/2, cursorY - CURSOR_SIZE/2,
+               CURSOR_SIZE, CURSOR_SIZE, TFT_RED);
+}
+
+void setup() {
+  init();
+  Serial.begin(9600);
+  uint16_t ID = tft.readID();    // read ID from display
+  Serial.print("ID = 0x");
+  Serial.println(ID, HEX);
+  if (ID == 0xD3D3) ID = 0x9481; // write-only shield  
+  tft.begin(ID);                 // LCD gets ready to work
+	Serial.print("Initializing SD card...");
+	if (!SD.begin(SD_CS)) {
+		Serial.println("failed! Is it inserted properly?");
+		while (true) {}
+	}
+	Serial.println("OK!");
+	tft.setRotation(1); tft.fillScreen(TFT_BLACK);
+	int yegMiddleX = YEG_SIZE/2 - (DISPLAY_WIDTH - 60)/2;
+	int yegMiddleY = YEG_SIZE/2 - DISPLAY_HEIGHT/2;
+	lcd_image_draw(&yegImage, &tft, yegMiddleX, yegMiddleY,
+                 0, 0, DISPLAY_WIDTH - 60, DISPLAY_HEIGHT);
+  // initial cursor position is the middle of the screen
+  cursorX = (DISPLAY_WIDTH - 60)/2;
+  cursorY = DISPLAY_HEIGHT/2;
+  redrawCursor();
+}
+
+// redraws map background on previous cursor position to remove black trail
+void redrawMap(uint16_t tempX, uint16_t tempY) {
+  int yegMiddleX = YEG_SIZE/2 - (DISPLAY_WIDTH - 60)/2;
+  int yegMiddleY = YEG_SIZE/2 - DISPLAY_HEIGHT/2;
+  int screenPatchX = tempX - CURSOR_SIZE/2;
+  // prevents cursor from moving to black column
+  screenPatchX = constrain(screenPatchX, 0, DISPLAY_WIDTH - 60 - CURSOR_SIZE);
+  int screenPatchY = tempY - CURSOR_SIZE/2;
+  lcd_image_draw(&yegImage, &tft, yegMiddleX + screenPatchX, 
+                  yegMiddleY + screenPatchY, screenPatchX, 
+                  screenPatchY, CURSOR_SIZE, CURSOR_SIZE);
+}
+// processes analog data and changes the increment 
+// depending on the read analog value (from +-1px to +-4px)
+void processAnalog(int aVal, char dir) {
+  int posBuffer = JOY_CENTER + JOY_DEADZONE;
+  int negBuffer = JOY_CENTER - JOY_DEADZONE;
+  int increment = 0;
+  if (aVal > posBuffer) {
+    increment += 1 + (aVal - posBuffer)/(JOY_DEADZONE * 2);
+  } else if (aVal < negBuffer) {
+    increment += -1 +(aVal - negBuffer)/(JOY_DEADZONE * 2);
+  }
+  // accounts for the inverted X increment
+  if (dir == 'X') {
+  	cursorX -= increment;
+  } else {
+  	cursorY += increment;	
+  }
+}
+
+void processJoystick() {
+  // temp variables to draw map on later
+  int tempX = cursorX, tempY = cursorY;
+  processAnalog(analogRead(JOY_HORIZ), 'X'); 
+  processAnalog(analogRead(JOY_VERT), 'Y');
+  int cRad = CURSOR_SIZE/2;
+  // subtract an extra 1 to account for the "centre" cursor pixel
+  cursorX = constrain(cursorX, cRad, DISPLAY_WIDTH - 61 - cRad);
+  cursorY = constrain(cursorY, cRad, DISPLAY_HEIGHT - 1 - cRad);
+  // will only redraw map when the cursor moves to prevent the cursor from flickering
+  if (tempX != cursorX || tempY != cursorY) {
+    redrawMap(tempX, tempY);
+    redrawCursor();
+  }
+}
+
