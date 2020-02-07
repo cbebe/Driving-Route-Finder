fb5908c361b1dd386adabf055d5881ae5496577c
poulomi-g <pganguly.6733@gmail.com>
2020-02-06 18:29:37 -0700

    hello

--- main.cpp
+++ main.cpp
@@ -5,18 +5,10 @@
 #include <SD.h>
 #include "joy_cursor.h"
 
-MCUFRIEND_kbv tft;
 void setup() {
   init();
   Serial.begin(9600);
   // sets up Arduino for Mode 0
-  uint16_t ID = tft.readID();    // read ID from display
-  if (ID == 0xD3D3) ID = 0x9481; // write-only shield  
-    tft.begin(ID);                 // LCD gets ready to work
-	if (!SD.begin(SD_CS)) {
-		while (true) {}
-	}
-	tft.setRotation(1); tft.fillScreen(TFT_BLACK);
   joySetup();
   pinMode(JOY_SEL, INPUT);
   digitalWrite(JOY_SEL, HIGH);
