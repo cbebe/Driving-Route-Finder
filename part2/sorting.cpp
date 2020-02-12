c3cb8083f9c5ad51dac7516a6259e191ba8c15c1
charlesancheta <cancheta@ualberta.ca>
2020-02-12 14:30:01 -0700

    quicksort done

--- part2/sorting.cpp
+++ part2/sorting.cpp
@@ -19,4 +19,26 @@ void iSort(RestDist array[], int length) {
   }
 }
 
-void partition(
+int pivot(RestDist a[], int length, int pi) {
+  swapRest(&a[pi], &a[length - 1]);
+  int lo = 0, hi = length - 2;
+  while (hi >= lo) {
+    if (a[lo].dist <= a[length - 1].dist) {
+      lo++;
+    } else if (a[hi].dist > a[length - 1].dist) {
+      hi--;
+    } else {
+      swapRest(&a[lo], &a[hi]);
+    }
+  }
+  swapRest(&a[lo], &a[length - 1]);
+  return lo;
+}
+
+void qSort(RestDist a[], int length) {
+  if (length<= 1) {return;}
+  int pi = length/2;
+  int new_pi = pivot(a, length, pi);
+  qSort(a, new_pi);
+  qSort(a, length - new_pi - 1);
+}
