72e5060948442ff30b2dc5f25208f45ff9a6d846
charlesancheta <cancheta@ualberta.ca>
2020-03-16 15:50:46 -0600

    eh

--- part2_client_base/serial_handling.cpp
+++ part2_client_base/serial_handling.cpp
@@ -3,14 +3,32 @@
 extern shared_vars shared;
 
 uint8_t get_waypoints(const lon_lat_32& start, const lon_lat_32& end) {
-  // Currently this does not communicate over the serial port.
-  // It just stores a path length of 0. You should make it communicate with
-  // the server over Serial using the protocol in the assignment description.
-
   // TODO: implement the communication protocol from the assignment
+  enum {sending, receiving} state = receiving; 
+  char serialmsg[]
+  while (state = receiving) {
+    // Send request to Serial
+    while (!Serial.available()) {
+      
+      Serial.write('R'); 
+      Serial.write(' '); int32_to_Serial(start.lon); 
+      Serial.write(' '); int32_to_Serial(start.lat);
+      Serial.write(' '); int32_to_Serial(end.lat);
+      Serial.write(' '); int32_to_Serial(end.lat);
+      Serial.write('\n');
+      // don't overflow the serial!
+      delay(500);
+    }
+    processSerial();
+    int16_t num_waypoints = char_to_int32(buffer, 1, 1); 
+    shared.num_waypoints = num;
+  }
+  
 
-  // for now, nothing is stored
-  shared.num_waypoints = 0;
+  
+
+
+
 
   // 1 indicates a successful exchange, of course you should only output 1
   // in your final solution if the exchange was indeed successful
