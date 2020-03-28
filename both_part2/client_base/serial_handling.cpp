dc6f75c9a4270ec417b68a9670a74e917b4b21c9
charlesancheta <cancheta@ualberta.ca>
2020-03-28 16:33:37 -0600

    done comms havent tested tho

--- both_part2/client_base/serial_handling.cpp
+++ both_part2/client_base/serial_handling.cpp
@@ -15,33 +15,68 @@ void sendReq(const lon_lat_32& start, const lon_lat_32& end) {
 // timeout function based on cmput274 assignment 2
 bool waitOnSerial() {
   uint32_t timeout = millis() + 1000;
-  while ((millis() < timeout && Serial.available() == 0) {delay(1);}
+  while (millis() < timeout && Serial.available() == 0) {delay(1);}
   return Serial.available() > 0;
 }
 // processes line from serial
 // returns false if nothing was received
 bool processLine(char *buff) {
-  int len = 0;
+  int len = 0; char c;
   if (waitOnSerial()) {
     do {
       c = Serial.read();
       // only read 24 characters and drop any excess
       if (len < 24) {
         buff[len] = c;
-        len++
+        len++;
         buff[len] = 0;
       }
-    } while ((c != '\n' || c != '\r') && waitOnSerial())
+    } while ((c != '\n' || c != '\r') && waitOnSerial());
     return true;
   }
   return false;
 }
 
-bool getNumWayPoints(char *buffer) {
+// get number of waypoints from serial
+// returns true if successful
+bool getNumWayPoints() {
+  char buffer[25];
   // restart if not a valid number of waypoints
-  if (buffer[0] != 'N') {return false;}
-  
-  return true;
+  if (processLine(buffer) && buffer[0] == 'N') {
+    // copy buffer to another char array
+    // assumes 500 is the max waypoints
+    char num[4]; num[3] = 0;
+    for (int i = 0; i < 3; i++) {
+      num[i] = buffer[i + 2];
+    }
+    shared.num_waypoints = atoi(num);
+    return true;
+  }
+  return false;
+}
+
+
+bool processWaypoint(int16_t index) {
+  char buff[25], lon[10], lat[10];
+  if (processLine(buff) && buff[0] == 'W') {
+    // assumes that all numbers would be the same format:
+    // 7 digits longitude 8 digits negative latitude
+    for (int i = 0; i < 10; i++) {
+      lon[i] = buff[i + 2];
+      lat[i] = buff[i + 10];
+    }
+    // add null terminators
+    lon[9] = 0; lat[9] = 0;
+
+    // store parsed coordinates to shared variable
+    shared.waypoints[index].lon = atoi(lon);
+    shared.waypoints[index].lat = atoi(lat);
+
+    // send acknowledge to serial
+    Serial.println("A");
+    return true;
+  }
+  return false;
 }
 
 uint8_t get_waypoints(const lon_lat_32& start, const lon_lat_32& end) {
@@ -50,16 +85,16 @@ uint8_t get_waypoints(const lon_lat_32& start, const lon_lat_32& end) {
   // the server over Serial using the protocol in the assignment description.
 
   // TODO: implement the communication protocol from the assignment
-
-  // for now, nothing is stored
-  shared.num_waypoints = 0;
+  // send request to serial
   sendReq(start, end);
-  // resend request when it times out (1 second)
-  if (!waitOnSerial()) {return 0;}
-  char buffer[25];
   // return 0 at any point that the communication fails
-  if (!processLine(buffer)) {return 0;}
-  if (!getNumWayPoints(buffer)) {return 0;}
+  if (!getNumWayPoints()) {return 0;}
+  // limits the number of waypoints from 0 to 500 (max_waypoints)
+  shared.num_waypoints = constrain(shared.num_waypoints, 0, max_waypoints);
+
+  for (int i = 0; i < shared.num_waypoints; i++) {
+    if (!processWayPoint(i)) {return 0;}
+  }
   // 1 indicates a successful exchange, of course you should only output 1
   // in your final solution if the exchange was indeed successful
   // (otherwise, return 0 if the communication failed)
--- test.cpp
+++ test.cpp
@@ -1,13 +1,47 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 void printTest() {
   int x = 12, y = 24;
   char c[50];
   sprintf(c, "W %d %d\n", x, y);
-  std::cout << c;
+  cout << c;
+}
+
+// discovered that anything that is not
+// int is dropped
+void atoiTest() {
+  cout << atoi("N 12\n") << endl;
+}
+
+void numTest() {
+  char *buffer = "N -500\n";
+  if (buffer[0] != 'N') {return;}
+  char num[5]; num[4] = 0;
+  for (int i = 0; i < 4; i++) {
+    num[i] = buffer[i + 2];
+  }
+  cout << atoi(num) << endl;
+  char *num1 = "500-";
+  cout << atoi(num1) << endl;
+}
+
+void processWaypoint() {
+  char lon[10], lat[10];
+  char buff[] = "W 5365488 -11333914";
+  // assumes that all numbers would be the same format:
+  // 7 digits longitude 8 digits negative latitude
+  for (int i = 0; i < 10; i++) {
+    lon[i] = buff[i + 2];
+    lat[i] = buff[i + 10];
+  }
+  lon[9] = 0; lat[9] = 0;
+  cout << atoi(lon) << endl;
+  cout << atoi(lat) << endl;
 }
 
 int main() {
-  std::cout << atoi("12\n") << std::endl;
+  processWaypoint();
   return 0;
 }
